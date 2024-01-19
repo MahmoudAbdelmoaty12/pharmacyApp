c5b498c7e58844f8a50acdc6a6cc931af5fd77bb
mahmoud <mahabdelmoaty00@gmail.com>
2024-01-19 02:22:59 +0200

    Committing local changes before pull

---
+++ PharmacyApp.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using pharmacyApp.Application.Contract;
+
+namespace PharmacyApp.Infrastructure.Repositories
+{
+   public  class Repository:IRepository<T,TId>
+    {
+
+
+
+
+    }
+}
